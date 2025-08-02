a91e903d05508528dd44cc82ce79c5278f87d687
Erlan123-vera <devildante238@gmail.com>
2025-08-02 00:02:32 -0400

    Add files via upload

---
+++ letra.js
@@ -0,0 +1,73 @@
+
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+var lyricsData = [
+  { text: "Él la estaba esperando con una flor amarilla", time: 13 },
+  { text: "Ella lo estaba soñando con la luz en su pupila", time: 23 },
+  { text: "Y el amarillo del sol iluminaba la esquina (la esquina)", time: 29 },
+  { text: "Lo sentía tan cercano, lo sentía desde niña", time: 39 },
+  { text: "Ella sabía que él sabía, que algún día pasaría", time: 43 },
+  { text: "Que vendría a buscarla, con sus flores amarillas", time: 49 },
+  { text: "No te apures no detengas, el instante del encuentro", time: 55 },
+  { text: "Está dicho que es un hecho, no la pierdas no hay derecho", time: 61 },
+  { text: "No te olvides, que la vida", time: 64 },
+  { text: "Casi nunca está dormida", time: 69 },
+  { text: "En ese bar tan desierto los esperaba el encuentro (el encuentro)", time: 91 },    
+  { text: "Ella llegó en limosina amarilla por supuesto", time: 102 },
+  { text: "Él se acercó de repente y la miró tan de frente (de frente)", time: 107 },
+  { text: "Toda una vida soñada y no pudo decir nada", time: 115 },
+  { text: "Ella sabía que él sabía, que algún día pasaría", time: 120 },
+  { text: "Que vendría a buscarla, con sus flores amarillas", time: 126 },
+  { text: "No te apures no detengas, el instante del encuentro", time: 134 },
+  { text: "Está dicho que es un hecho, no la pierdas no hay derecho", time: 138},
+  { text: "No te olvides, que la vida", time: 143 },
+  { text: "Casi nunca está dormida", time: 147 },
+  { text: "(Flores amarillas)", time: 158 },
+  { text: "Ella sabía que él sabía, que algún día pasaría", time: 167 },
+  { text: "Que vendría a buscarla, con sus flores amarillas", time: 171 },
+  { text: "No te apures no detengas, el instante del encuentro", time: 180 },
+  { text: "Está dicho que es un hecho, no la pierdas no hay derecho", time: 184 },
+  { text: "No te olvides, que la vida", time: 189 },
+  { text: "Casi nunca está dormida", time:190},
+  { text: "Ella sabía, que él sabía", time: 194 },
+  { text: "Él sabía, ella sabia", time:195 },
+  { text: "Él sabía, ella sabía y se olvidaron, de sus flores amarillas", time:197 },
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
